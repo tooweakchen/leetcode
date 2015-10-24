bc869050b877c95a6a330f4ce4f5251a93b175b5
tooweakchen <1292095940@qq.com>
2015-10-24 14:52:31 +0800

    第33题

---
+++ leetcode33.py
@@ -0,0 +1,13 @@
+#__author__ = 'tooweakchen'
+#coding:utf-8
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
+        return -1
