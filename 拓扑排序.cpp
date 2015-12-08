ad8bd1b3e521cf4d4fbc2a485178845b9f105e58
tooweakchen <1292095940@qq.com>
2015-12-08 22:30:16 +0800

    拓扑排序

---
+++ 拓扑排序.cpp
@@ -0,0 +1,19 @@
+/**
+    拓扑排序
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+const int maxn=500+10;
+int in[maxn];
+int n,m;
+
+int main()
+{
+    while(scanf("%"))
+}
